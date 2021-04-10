15201450ff06b543f587f653df9882a90786051e
Deedee N <you@example.com>
2021-04-10 18:12:28 +0200

    Implemented feedback on ex1 & ex2

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -15,49 +15,34 @@
    url with `.shx`. There is no server at the modified url, therefore this 
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
-function requestData(url) {
+async function requestData(url) {
   // TODO return a promise using `fetch()`
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log('data', data);
-      return data;
-    })
-    .catch((error) => {
-      console.log('err', error);
-    });
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-function renderImage() {
+function renderImage(data) {
   // TODO render the image to the DOM
-  fetch('https://xkcd.now.sh/?comic=latest')
-    .then((resp) => resp.json())
-    .then((data) => {
-      const img = document.createElement('img');
-      img.src = data.img;
-      document.body.appendChild(img);
-      return img;
-    });
+  const img = document.createElement('img');
+  img.src = data.img;
+  document.body.appendChild(img);
 }
 
 function renderError(error) {
   // TODO render the error to the DOM
-  if (error) {
-    const h1 = document.createElement('h1');
-    h1.textContent = 'something went wrong!';
-    document.body.appendChild(h1);
-  }
+  const h1 = document.createElement('h1');
+  h1.textContent = error.message;
+  document.body.appendChild(h1);
 }
 
 // TODO refactor with async/await and try/catch
 async function main() {
+  const data = await requestData('https://xkcd.now.sh/?comic=latest');
   try {
-    const data = await requestData('https://xkcd.now.sh/?comic=latest');
     renderImage(data);
   } catch (error) {
-    renderError(error);
+    renderError(error.message);
   }
 }
 
--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -22,22 +22,21 @@ async function fetchData(url) {
   // TODO complete this function
   const response = await fetch(url);
   const data = await response.json();
-  return data.results;
+  return data;
 }
 
-function fetchAndPopulatePokemons(data) {
+function fetchAndPopulatePokemons(select, data) {
   // TODO complete this function
-  const select = document.getElementById('select');
-  data.forEach((pokemon) => {
+  const pokemons = data.results;
+  pokemons.forEach((pokemon) => {
     const option = document.createElement('option');
     select.appendChild(option);
     option.value = pokemon.name;
     option.textContent = pokemon.name;
   });
-  select.addEventListener('change', fetchImage);
 }
 
-async function fetchImage() {
+async function fetchImage(select) {
   // TODO complete this function
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${select.value}`
@@ -47,17 +46,39 @@ async function fetchImage() {
   img.src = data.sprites.front_default;
   document.body.appendChild(img);
   img.style.width = '200px';
+
+  const reset = () => {
+    img.style.display = 'none';
+  };
+  select.addEventListener('change', reset);
 }
 
 async function main() {
   // TODO complete this function
-  try {
-    const data = await fetchData(
-      'https://pokeapi.co/api/v2/pokemon/?limit=151'
-    );
-    fetchAndPopulatePokemons(data);
-  } catch (error) {
-    console.log(error);
-  }
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+
+  const button = document.createElement('button');
+  div.appendChild(button);
+  button.setAttribute('type', 'button');
+  button.textContent = 'Get pokemon';
+
+  const select = document.createElement('select');
+  div.appendChild(select);
+
+  button.addEventListener('click', async () => {
+    try {
+      const data = await fetchData(
+        'https://pokeapi.co/api/v2/pokemon/?limit=151'
+      );
+      fetchAndPopulatePokemons(select, data);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  select.addEventListener('change', () => {
+    fetchImage(select);
+  });
 }
 window.addEventListener('load', main);
